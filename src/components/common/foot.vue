10adeed165a589b9e9c6d9885b21f1d787d858e8
guoyi <1806242240@qq.com>
2020-02-29 17:35:11 +0800

    Update foot.vue

--- src/components/common/foot.vue
+++ src/components/common/foot.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="github-footer">
-    <a class="footer-link" href="https://github.com/gwuhaolin/resume">页面托管在Github Pages上</a>
+    <a class="footer-link" href="https://github.com/GuoyiZhang/078-Vue-resumeVue">页面托管在Github Pages上</a>
   </footer>
 </template>
 <script>
